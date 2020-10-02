7e7ce3834889ae1b37753d3f86d1d34b674101ff
RichardBongani <phalarichard1@gmail.com>
2020-10-02 14:43:44 +0200

    initial commit with static layout information

--- README.md
+++
@@ -1,2 +0,0 @@
-# School_rails_site
-This is a School website containing a Dashboard and User Authentification
---
+++ School_site/app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+    def index
+        @title1= 'Hlomphanang College'
+        @title2= 'Ring Commitment'
+    end
+
+    def about
+    end
+
+    def services
+    end
+
+    def contact
+    end
+
+end
