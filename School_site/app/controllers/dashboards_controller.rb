9d2ce4db93eaef3b9a7a2860fcf77929452637c4
RichardBongani <phalarichard1@gmail.com>
2020-10-05 23:13:19 +0200

    modifying routes  & controller

--- School_site/app/controllers/dashboards_controller.rb
+++ School_site/app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   before_action :set_dashboard, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /dashboards
   # GET /dashboards.json
--- School_site/config/routes.rb
+++ School_site/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :dashboards
   devise_for :users
-
+  resources :dashboards
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'pages#index', as: 'home'
 get 'about' => 'pages#about', as: 'about'
