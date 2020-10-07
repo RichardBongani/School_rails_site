3a5ca2da595d1e3bb57cd40de47f7f9975e75648
RichardBongani <phalarichard1@gmail.com>
2020-10-07 18:58:45 +0200

    configuring the application

--- School_site/app/controllers/registration_controller.rb
+++ School_site/app/controllers/registration_controller.rb
@@ -1,11 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
-    private
+    # def index 
+    #     @users = User.all
+    # end
+    # private
 
-    def sign_up_params
-        params.require(:user).permit(:name, :username, :phone, :studentnumber, :email, :password, :password_confirmation)
-    end
+    # def sign_up_params
+    #     params.require(:user).permit(:name, :username, :phone, :studentnumber, :email, :password, :password_confirmation)
+    # end
 
-    def account_update_params
-        params.require(:user).permit(:name, :username, :phone, :studentnumber, :email, :password, :password_confirmation, :current_password)
-    end
+    # def account_update_params
+    #     params.require(:user).permit(:name, :username, :phone, :studentnumber, :email, :password, :password_confirmation, :current_password)
+    # end
 end
