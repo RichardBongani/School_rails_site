9b44aee23f153f244d87dc6ede9ab7bf138da40c
RichardBongani <phalarichard1@gmail.com>
2020-10-08 21:52:53 +0200

    adding default avatar

--- School_site/app/models/user.rb
+++ School_site/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_secure_password
 
   validates :email, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
@@ -7,6 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  after_commit :add_default_avatar, on: %i[create update]
 
   def avatar_thumbnail
     if avatar.attached?
@@ -16,4 +16,17 @@ class User < ApplicationRecord
     end
   end
 
+  private
+  def add_default_avatar
+    unless avatar.attached?
+       avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_profile.png'
+          )
+        ), filename: 'default_profile.png',
+        content_type: 'image/png'
+      )
+    end
+  end
 end
