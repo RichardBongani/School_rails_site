3b7bc65df41b8ba00689c002800b1150f299ca1e
RichardBongani <phalarichard1@gmail.com>
2020-10-08 16:42:35 +0200

    modifying routes, helpers, navbars, devise

--- School_site/config/routes.rb
+++ School_site/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   root to: 'dashboard#index'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
