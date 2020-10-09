146a3166547b1c74de0702b7c586df3a47818b90
RichardBongani <phalarichard1@gmail.com>
2020-10-09 14:23:01 +0200

    modifying error messages and sign up form styling and layout

--- School_site/app/models/user.rb
+++ School_site/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   validates :email, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
